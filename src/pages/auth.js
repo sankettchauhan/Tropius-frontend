07ea3d8fbe66bb9dc6f77bc4a40e3a8e3a018979
sankettchauhan <sanket.chauhan4@gmail.com>
2021-10-08 17:34:50 +0530

    added form validation

--- src/pages/auth.js
+++ src/pages/auth.js
@@ -5,18 +5,15 @@ import {
   Grid,
   Typography,
   Container,
+  LinearProgress,
+  Box,
 } from "@material-ui/core";
-import TextField from "../components/common/TextInput";
+import MuiTextField from "@material-ui/core/TextField";
+import CustomTextField from "../components/common/TextInput";
 import { makeStyles } from "@material-ui/core/styles";
 import { authenticateUser, createUser } from "../axios/user";
 import { Formik, Form, Field } from "formik";
-import {
-  fieldToTextField,
-  TextField,
-  TextFieldProps,
-  Select,
-  Switch,
-} from "formik-material-ui";
+import { fieldToTextField, TextFieldProps } from "formik-material-ui";
 const _ = require("lodash");
 
 const useStyles = makeStyles((theme) => ({
@@ -37,7 +34,34 @@ const useStyles = makeStyles((theme) => ({
     display: "grid",
     placeItems: "center",
   },
+  textinput: {
+    marginBottom: theme.spacing(2),
+  },
 }));
+
+function TextField(props) {
+  const {
+    form: { setFieldValue },
+    field: { name },
+  } = props;
+  // const onChange = React.useCallback(
+  //   (event) => {
+  //     const { value } = event.target;
+  //     setFieldValue(name, value ? value.toUpperCase() : "");
+  //   },
+  //   [setFieldValue, name]
+  // );
+  const classes = useStyles();
+  return (
+    <MuiTextField
+      {...fieldToTextField(props)}
+      // onChange={onChange}
+      variant="outlined"
+      className={classes.textinput}
+      fullWidth
+    />
+  );
+}
 
 export default function Auth({ history }) {
   const classes = useStyles();
@@ -52,7 +76,7 @@ export default function Auth({ history }) {
   const handleClick = () => setLogin((state) => !state);
 
   const handleSubmit = async (e) => {
-    e.preventDefault();
+    // e.preventDefault();
     let user = {
       email: form.email,
       password: form.password,
@@ -68,93 +92,110 @@ export default function Auth({ history }) {
     <Container component="main" maxWidth="xs" className={classes.container}>
       <CssBaseline />
       <div className={classes.paper}>
-        <Typography component="h1" variant="h5" align="center">
+        <Typography component="h1" variant="h5" align="center" gutterBottom>
           {login ? "Login" : "Sign up"}
         </Typography>
-        <form className={classes.form} noValidate onSubmit={handleSubmit}>
-          <Grid container spacing={2}>
-            {!login && (
-              <>
-                <Grid item xs={12} sm={6}>
-                  <TextField
-                    label="First Name"
-                    value={form.firstname}
-                    onChange={(e) =>
-                      setForm((state) => ({
-                        ...state,
-                        firstname: e.target.value,
-                      }))
-                    }
-                    autoFocus
-                  />
-                </Grid>
-                <Grid item xs={12} sm={6}>
-                  <TextField
-                    label="Last Name"
-                    value={form.lastname}
-                    onChange={(e) =>
-                      setForm((state) => ({
-                        ...state,
-                        lastname: e.target.value,
-                      }))
-                    }
-                  />
-                </Grid>
-              </>
-            )}
-            <Grid item xs={12}>
-              <TextField
+
+        <Formik
+          initialValues={{
+            email: "",
+            password: "",
+            firstname: "",
+            lastname: "",
+          }}
+          validate={(values) => {
+            const errors = {};
+            if (!login) {
+              if (!values.firstname) {
+                errors.firstname = "Required";
+              }
+              if (!values.lastname) {
+                errors.lastname = "Required";
+              }
+            }
+            if (!values.email) {
+              errors.email = "Required";
+            } else if (
+              !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)
+            ) {
+              errors.email = "Invalid email address";
+            }
+            if (!values.password) {
+              errors.password = "Required";
+            }
+            return errors;
+          }}
+          onSubmit={(values, { setSubmitting }) => {
+            console.log(values);
+            // handleSubmit(values);
+            setSubmitting(false);
+          }}
+        >
+          {({ submitForm, isSubmitting }) => (
+            <Form>
+              {!login && (
+                <Field
+                  component={TextField}
+                  name="firstname"
+                  type="firstname"
+                  label="Firstname"
+                />
+              )}
+              {!login && (
+                <Field
+                  component={TextField}
+                  name="lastname"
+                  type="lastname"
+                  label="Lastname"
+                />
+              )}
+              <Field
+                component={TextField}
+                name="email"
                 type="email"
                 label="Email"
-                value={form.email}
-                autoFocus={login}
-                onChange={(e) =>
-                  setForm((state) => ({ ...state, email: e.target.value }))
-                }
               />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                label="Password"
-                value={form.password}
-                onChange={(e) =>
-                  setForm((state) => ({ ...state, password: e.target.value }))
-                }
+              <Field
+                component={TextField}
                 type="password"
+                label="Password"
+                name="password"
               />
-            </Grid>
-          </Grid>
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            color="primary"
-            className={classes.submit}
-          >
-            {login ? "Login" : "Sign up"}
-          </Button>
-          <Grid container justify="flex-end">
-            <Grid item>
-              <Typography className={classes.text} color="primary">
-                {login ? (
-                  <>
-                    Don't have an account?{" "}
-                    <span className={classes.link} onClick={handleClick}>
-                      Create one
-                    </span>
-                  </>
-                ) : (
-                  <>
-                    Already have an account?{" "}
-                    <span className={classes.link} onClick={handleClick}>
-                      Login
-                    </span>
-                  </>
-                )}
+              {isSubmitting && <LinearProgress />}
+              <Typography align="right" gutterBottom>
+                <Button
+                  variant="contained"
+                  color="primary"
+                  disabled={isSubmitting}
+                  onClick={submitForm}
+                >
+                  Submit
+                </Button>
               </Typography>
-            </Grid>
+            </Form>
+          )}
+        </Formik>
+        <Grid container justify="flex-end">
+          <Grid item>
+            <Typography className={classes.text} color="primary">
+              {login ? (
+                <>
+                  Don't have an account?{" "}
+                  <span className={classes.link} onClick={handleClick}>
+                    Create one
+                  </span>
+                </>
+              ) : (
+                <>
+                  Already have an account?{" "}
+                  <span className={classes.link} onClick={handleClick}>
+                    Login
+                  </span>
+                </>
+              )}
+            </Typography>
           </Grid>
-        </form>
+        </Grid>
       </div>
     </Container>
   );
